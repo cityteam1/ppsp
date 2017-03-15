5a4942107833dc3fe9f3d379fd81b1b6074a71da
danny0533 <Danny@Danny-MacBook-Pro.local>
2017-03-15 16:01:10 +0800

    New controller

---
+++ application/controllers/Index.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once "Users.php";
+class Index extends Users{
+    	public function __construct() {
+		
+		parent::__construct();
+		$this->load->library(array('session'));
+		$this->load->helper(array('url'));
+		$this->load->model('user_model');
+		
+	}
+	
+        public function index() {
+		$this->load->view('header');
+		$this->load->view('index');
+		$this->load->view('footer');
+	}
+}
+
+
+?>
